b20d42d5f2257c90658a769dadb1f1b1f0162941
Safiyya H <safiyya.hasanova@gmail.com>
2021-07-24 18:25:22 -0500

    final fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const {db} = require ('./db/db.json')
-const createNewNote = require ('./lib/notes')
-const path= require ('path');
+const path = require('path');
 const express = require('express');
+const { db } = require('./db/db.json');
+const createNewNote = require('./lib/notes');
 const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
@@ -11,15 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// route to render functional page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-// route to render landing page
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 
 // route to render 'notes' data
@@ -35,6 +26,15 @@ app.post('/api/notes', (req, res) => {
 });
 
 
+// route to render functional page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// route to render landing page
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
