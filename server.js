6b324b882ccae8cf4c8eac12d99f38bb89d7a773
Safiyya H <safiyya.hasanova@gmail.com>
2021-07-24 17:52:49 -0500

    api routs

--- server.js
+++ server.js
@@ -10,13 +10,26 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // route to render functional page
-app.get('/note', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // route to render landing page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+// route to render 'notes' data
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// route to add new data to 'notes'
+app.post('/api/notes', (req, res) => {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
 });
 
 
